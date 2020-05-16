a413a8f93211331225d7d4e670f0c08d02f4cfcd
Mehmet Ali Özer <mehmetali.ozer@std.yeditepe.edu.tr>
2020-05-16 11:49:40 +0300

    gui update

--- gui_showcase/main.py
+++ gui_showcase/main.py
@@ -12,27 +12,42 @@ inzva_logo = ImageTk.PhotoImage(logo_image_path)
 inzva_logo_label = Label(image=inzva_logo).pack()
 
 #window size
-gui.geometry("800x600")
+canvas = Canvas(gui, width=800, height=600)
+canvas.pack()
 
 #title
 rodnet_label = Label(gui, text="RODNET Robust Object Detection Beta Showcase: ",font=("Helvetica", 12)).pack()
+
+def change(image_new):
+    canvas.itemconfig(image_id, image=image_new)
 
 def open():
     global predicted_image
     gui.filename = filedialog.askopenfilename(initialdir="/", title="Select a File", filetypes=(("png files", "*.png"),("all files", "*.*")))
     
     if(gui.filename):
+        global image2
         print(gui.filename) #image path on local pc
         #call predictor with (gui.filename)
+        image2_open = Image.open(gui.filename)
+        w = int(image2_open.width * 0.5)
+        h = int(image2_open.height * 0.5)
+        print(w,h)
+        image2_open = image2_open.resize((w,h), Image.ANTIALIAS)
 
-        #get predicted photo file path show as below
-        #image_path = Image.open(prediction(gui.filename))
+        image2 = ImageTk.PhotoImage(image2_open)
+        
+        #update canvas
+        change(image2)
 
-        image_path = Image.open(gui.filename)
-        predicted_image = ImageTk.PhotoImage(image_path)
-        my_image_label = Label(image=predicted_image).pack()
     else:
         print("No selection")
+
+
+image_showcase = PhotoImage(file="./images/rodnet_logo.png")
+image_id = canvas.create_image(0, 0, anchor='nw', image=image_showcase)
+
+
 
 open_label = Label(gui, text="Select the image: ").pack()
 open_button = Button(gui, text="Open File", command=open).pack()
